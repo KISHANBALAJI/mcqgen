8c2dd40c2e5ee9b95d749881b068c18ce503a621
KISHANBALAJI <kishanbalajimeruga@gmail.com>
2024-07-20 09:33:57 +0530

    path updated

--- streamlitAPP.py
+++ streamlitAPP.py
@@ -5,12 +5,12 @@ import pandas as pd
 from dotenv import load_dotenv
 from src.mcqgenerator.utils import read_file,get_table_data
 import streamlit as st
-from langchain.callbacks import get_openai_callback
+from langchain.callbacks import get_openai_callback # type: ignore
 from src.mcqgenerator.MCCQgenerator import generate_evaluate_chain
 from src.mcqgenerator.logger import logging
 
 #loading json file
-with open('C:\Users\kisha\mcqgen\Response.json','r')as file:
+with open("C:\\Users\\kisha\\mcqgen\\Response.json",'r')as file:
     RESPONSE_JSON = json.load(file)
 
 #creating a title for the app

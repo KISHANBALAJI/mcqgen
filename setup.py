8750a8a668bda30b6b09b00c85b65a67a2353001
KISHANBALAJI <kishanbalajimeruga@gmail.com>
2024-07-20 07:41:01 +0530

    logger added

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = '0.0.1',
     author = 'Kishan Balaji',
     author_email = 'kishanbalajimeruga@gmail.com',
-    install_requires =["openai","langchain","stramlit","python-dotenv","PyPDF2"],
+    install_requires =["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages = find_packages()
 )
+#python setup.py install
